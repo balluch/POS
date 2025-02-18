17789d7c0c8e5c91c97a917327ef9b98dda7377b
balluch <balluch@spengergasse.at>
2025-02-18 21:12:13 +0100

    Commit 20250218

--- 06 REST/01_GetRoutes/GetRoutesDemo/GetRoutesDemo/Program.cs
+++ 06 REST/01_GetRoutes/GetRoutesDemo/GetRoutesDemo/Program.cs
@@ -4,15 +4,20 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+// Add services to the container.
 
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
